f1444c88b7814876a77c6509aaa0c93981ebb0e3
Nicholas Riley <com-github@sabi.net>
2020-08-01 16:45:24 -0400

    Keep preferred microphone active where possible.

--- misc/microphone.py
+++ misc/microphone.py
@@ -1,8 +1,24 @@
 from talon import actions
 from talon.microphone import manager
 
+PREFERRED_MICROPHONES = ('Jabra Link 370', )
+
 def mic_changed_to(device):
-    if device and device.name not in ('Jabra Link 370', ):
+    if device and device.name not in PREFERRED_MICROPHONES:
         actions.speech.disable()
 
 manager.register('mic_change', mic_changed_to)
+
+# note: cubeb API may not be stable; don't rely on this
+from talon.lib import cubeb
+from talon import scripting
+
+ctx = cubeb.Context()
+def devices_changed(device_type):
+    if device_type is cubeb.DeviceType.INPUT:
+        for device in ctx.inputs():
+            if device.name in PREFERRED_MICROPHONES:
+                scripting.global_speech_system.set_microphone(device)
+                return
+
+ctx.register('devices_changed', devices_changed)
