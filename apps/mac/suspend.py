f237e2a222c58a127a846859be6b059458734f69
Nicholas Riley <com-github@sabi.net>
2020-09-03 13:22:20 -0400

    Suspend Talon in conferencing apps, not just Citrix.

--- apps/mac/suspend.py
+++ apps/mac/suspend.py
@@ -2,7 +2,11 @@ from talon import actions, ui
 was_enabled = False
 def fn(_):
     global was_enabled
-    if ui.active_app().bundle == 'com.citrix.XenAppViewer' and actions.speech.enabled():
+    if ui.active_app().bundle in (
+        'com.citrix.XenAppViewer',
+        'com.webex.meetingmanager',
+        'com.microsoft.teams'
+        ) and actions.speech.enabled():
         was_enabled = True
         actions.user.switcher_hide_running()
         actions.user.history_disable()
