d7820f1a2d7c3a5b8f6a9325b3ea7f683ba0d693
Nicholas Riley <nriley@metrohealth.org>
2020-09-20 16:53:37 -0400

    Disable Fluency Direct from Talon.

---
+++ misc/fd.py
@@ -0,0 +1,22 @@
+import win32api
+import win32com.client
+
+win32api.SetDllDirectory(r'C:\MModal\Server')
+
+fda = win32com.client.Dispatch("FDLink.Application")
+
+def disable_fd():
+	fdas = fda.Connect()
+	if fdas is None:
+		return # unable to connect
+
+	# this just turns on/off dictation; leaves commands enabled
+	# fddc = fdas.GetDictationControl()
+	# fddc.SetEnabled(True)
+
+	# can turn OFF microphone this way; turning it back on does nothing
+	fdrc = fdas.GetRecordingControl()
+	fdrc.EnableRecording(False)
+
+if __name__ == '__main__':
+	disable_fd()
