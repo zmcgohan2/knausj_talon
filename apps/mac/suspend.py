72731ca0d3c1dd134863e80e282247ba5f1fc2d8
Nicholas Riley <com-github@sabi.net>
2020-10-09 12:29:08 -0400

    Allow enabling Talon dictation in apps where it is otherwise suspended (mostly useful for Citrix Workspace), and having it "stick".

--- apps/mac/suspend.py
+++ apps/mac/suspend.py
@@ -1,21 +1,37 @@
 from talon import actions, ui
-was_enabled = False
+
+DEFAULT_DISABLE_BUNDLE_IDS = frozenset({
+    'com.apple.FaceTime',
+    'com.citrix.XenAppViewer',
+    'com.webex.meetingmanager',
+    'com.microsoft.teams'
+})
+
+was_enabled_globally = False
+was_enabled_in_app = set()
+last_active_app_bundle_id = None
+
 def fn(_):
-    global was_enabled
-    if ui.active_app().bundle in (
-        'com.apple.FaceTime',
-        'com.citrix.XenAppViewer',
-        'com.webex.meetingmanager',
-        'com.microsoft.teams'
-        ) and actions.speech.enabled():
-        was_enabled = True
-        actions.user.switcher_hide_running()
-        actions.user.history_disable()
-        actions.user.homophones_hide()
-        actions.user.help_hide()
-        actions.user.mouse_sleep()
-        actions.speech.disable()
-    elif was_enabled:
-        was_enabled = False
+    global was_enabled_globally, last_active_app_bundle_id
+    active_app_bundle_id = ui.active_app().bundle
+    if actions.speech.enabled():
+        if active_app_bundle_id in DEFAULT_DISABLE_BUNDLE_IDS:
+            was_enabled_globally = True
+            if active_app_bundle_id not in was_enabled_in_app:
+                actions.user.switcher_hide_running()
+                actions.user.history_disable()
+                actions.user.homophones_hide()
+                actions.user.help_hide()
+                actions.user.mouse_sleep()
+                actions.speech.disable()
+        elif last_active_app_bundle_id in DEFAULT_DISABLE_BUNDLE_IDS:
+            was_enabled_in_app.add(last_active_app_bundle_id)
+    elif last_active_app_bundle_id in DEFAULT_DISABLE_BUNDLE_IDS:
+        was_enabled_in_app.discard(last_active_app_bundle_id)
+        was_enabled_globally = False
+    elif was_enabled_globally:
+        was_enabled_globally = False
         actions.speech.enable()
+    last_active_app_bundle_id = active_app_bundle_id
+    
 ui.register('app_activate', fn)
