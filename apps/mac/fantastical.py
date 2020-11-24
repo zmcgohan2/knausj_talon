ee30aa3fab0d9a804126a1d5bf63a1a48457faf0
Nicholas Riley <com-github@sabi.net>
2020-11-24 10:43:13 -0500

    Fantastical calendar display.

--- apps/mac/fantastical.py
+++ apps/mac/fantastical.py
@@ -14,6 +14,16 @@ class fantastical_actions:
 		text = text.replace('"', '\"')
 		applescript.run(f'tell app id "com.flexibits.fantastical2.mac" to parse sentence "{text}"')
 
+	def fantastical_show_mini_calendar():
+		import webbrowser
+		webbrowser.open(f"x-fantastical3://show/mini")
+
+	def fantastical_show_calendar():
+		import webbrowser
+		webbrowser.open(f"x-fantastical3://show/calendar")
+
 @mod.action_class
 class FantasticalActions:
 	def fantastical_parse(text: str): """Parses the text in Fantastical."""
+	def fantastical_show_mini_calendar(): """Shows the mini calendar popover."""
+	def fantastical_show_calendar(): """Shows the calendar window."""
--- apps/mac/fantastical.talon
+++ apps/mac/fantastical.talon
@@ -2,3 +2,9 @@ os: mac
 -
 fantastical <user.text>:
 	user.fantastical_parse(user.formatted_text(user.text, 'CAPITALIZE_FIRST_WORD'))
+
+calendar mini:
+	user.fantastical_show_mini_calendar()
+
+calendar open:
+	user.fantastical_show_calendar()
