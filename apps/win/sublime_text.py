fc77936a204aa4710b5e5c6e42da7c3f7eb009b1
Nicholas Riley <nriley@metrohealth.org>
2020-12-11 16:18:58 -0500

    Port Sublime Text support to Windows.

---
+++ apps/win/sublime_text.py
@@ -0,0 +1,52 @@
+from talon import Context, actions, Module
+
+mod = Module()
+ctx = Context()
+
+ctx.matches = r"""
+os: windows
+and app.name: Sublime Text
+"""
+
+@ctx.action_class("edit")
+class edit_actions:
+	def jump_line(n: int):
+		actions.key("ctrl-g")
+		actions.insert(str(n))
+		actions.key("enter")
+
+@ctx.action_class("user")
+class user_actions:
+	def find(text: str):
+		actions.key("ctrl-f")
+		actions.insert(text)
+
+	def find_everywhere(text: str):
+		actions.key("ctrl-shift-f")
+		actions.insert(text)
+
+	def replace(text: str): 
+		actions.key("ctrl-h")
+		actions.insert(text)
+
+	replace_everywhere = find_everywhere
+
+	def select_previous_occurrence(text: str):
+		actions.key("ctrl-i")
+		actions.insert(text)
+		actions.key("shift-enter")
+
+	def select_next_occurrence(text: str):
+		actions.key("ctrl-i")
+		actions.insert(text)
+		actions.key("enter")
+
+@ctx.action_class("win")
+class win_actions:
+	def filename():
+		title = actions.win.title()
+		result = title.split(" — ")[0]
+		return result if "." in result else ""
+
+	def file_ext():
+		return actions.win.filename().split(".")[-1]
