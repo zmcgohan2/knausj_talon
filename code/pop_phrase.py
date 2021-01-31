95269edff37a72d436804c6a2ab0450fdac2a96b
Nicholas Riley <com-github@sabi.net>
2021-01-31 11:37:06 -0500

    Pop to repeat navigation commands.

---
+++ code/pop_phrase.py
@@ -0,0 +1,43 @@
+# from mankoff on Talon Slack
+
+from talon import Module, Context, app, speech_system, actions, noise
+
+ctx = Context()
+mod = Module()
+
+last_phrase = ""
+pop_phrase = ""
+
+auto_pop_that_phrases = ["go"]
+
+def on_phrase(j):
+    """Record the last phrase"""
+    phrase = getattr(j["parsed"], "_unmapped", j["phrase"])
+    phrase = " ".join(word.split("\\")[0] for word in phrase)
+    global last_phrase
+    last_phrase = phrase
+    print(f'*** Storing last phrase: {last_phrase} ***')
+    # auto-set pop_phrase if the last phrase is in the auto_pop_that_phrases list
+
+    global auto_pop_that_phrases
+    for p in auto_pop_that_phrases:
+        if phrase[0:len(p)] == p:
+            global pop_phrase
+            pop_phrase = phrase
+
+speech_system.register('post:phrase', on_phrase)
+
+@mod.action_class
+class Actions:
+    def pop_phrase():
+        """Set pop command to the last phrase"""
+        global last_phrase
+        global pop_phrase
+        pop_phrase = last_phrase
+        print(f'*** Setting pop phrase: {pop_phrase} ***')
+
+def on_pop(active):
+    global pop_phrase
+    actions.mimic(pop_phrase) 
+
+noise.register("pop", on_pop)
