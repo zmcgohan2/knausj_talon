14a5f52cc73bad00e91658ed6a5b82e24c545b8c
knausj85 <knaus.jeff@gmail.com>
2021-02-17 07:53:13 -0700

    Update keys.py

--- code/keys.py
+++ code/keys.py
@@ -3,7 +3,7 @@ from typing import Set
 from talon import Module, Context, actions, app
 import sys
 
-default_alphabet = "air bat cap drum each fine gust harp sit jury crunch look made near opt pit quench red sun trap urge vest whale plex yank zip".split(
+default_alphabet = "air bat cap drum each fine gut harp sit jury crunch look made near opt pit quench red sun trap urge vest whale plex yank zip".split(
     " "
 )
 letters_string = "abcdefghijklmnopqrstuvwxyz"
@@ -199,7 +199,7 @@ symbol_key_words = {
     "asterisk": "*",
     # "pound": "#",
     "hash": "#",
-    "percent": "%",
+    "percy": "%",
     "caret": "^",
     "amper": "&",
     "pipe key": "|",
