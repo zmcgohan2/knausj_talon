9b8e81c8ae775047eecc62dfdbf12b121a9a3a70
knausj85 <knaus.jeff@gmail.com>
2021-02-16 20:32:00 -0700

    Update file_manager.py

--- code/file_manager.py
+++ code/file_manager.py
@@ -347,7 +347,7 @@ def update_lists():
     files = {}
     folder_selections = []
     file_selections = []
-    print(path)
+    # print(path)
     try:
         current_path = Path(path)
         is_valid_path = current_path.is_dir()
@@ -355,7 +355,7 @@ def update_lists():
         is_valid_path = False
 
     if is_valid_path:
-        print("valid..." + str(current_path))
+        # print("valid..." + str(current_path))
         try:
             directories = get_directory_map(current_path)
             files = get_file_map(current_path)
