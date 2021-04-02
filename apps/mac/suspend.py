90ed3d6b32aca8385bd38f5c853acd425c3d54bd
Nicholas Riley <com-github@sabi.net>
2021-04-02 12:54:07 -0400

    Change Talon suspension in conferencing apps to work even when the apps are not frontmost.
    As it was, things would often go very badly during screen sharing sessions.
    
    Also add Zoom.

--- apps/mac/suspend.py
+++ apps/mac/suspend.py
@@ -3,39 +3,48 @@ from talon import actions, ui
 DEFAULT_DISABLE_BUNDLE_IDS = frozenset({
     'com.apple.FaceTime',
     'com.webex.meetingmanager',
-    'com.microsoft.teams'
+    'com.microsoft.teams',
+    'us.zoom.xos'
 })
 
 was_enabled_globally = False
-was_enabled_in_app = set()
-last_active_app_bundle_id = None
+disabling_app_bundle_ids = set()
 
-def fn(_):
-    global was_enabled_globally, last_active_app_bundle_id
-    active_app_bundle_id = ui.active_app().bundle
-    # print(f'was_enabled_globally: {was_enabled_globally}; active_app_bundle_id: {active_app_bundle_id}')
-    # print(f'was_enabled_in_app: {was_enabled_in_app}')
-    if actions.speech.enabled():
-        if active_app_bundle_id in DEFAULT_DISABLE_BUNDLE_IDS:
-            was_enabled_globally = True
-            if active_app_bundle_id not in was_enabled_in_app:
-                actions.user.switcher_hide_running()
-                actions.user.history_disable()
-                actions.user.homophones_hide()
-                actions.user.help_hide()
-                actions.speech.disable()
-        elif last_active_app_bundle_id in DEFAULT_DISABLE_BUNDLE_IDS:
-            was_enabled_in_app.add(last_active_app_bundle_id)
-    else:
-        if was_enabled_globally:
-            was_enabled_globally = False
-            # print(f'enabling...')
+def app_launched(app):
+    global was_enabled_globally, disabling_app_bundle_ids
+    launched_app_bundle_id = app.bundle
+    print(f'launched {launched_app_bundle_id}')
+    print(f'was_enabled_globally: {was_enabled_globally}')
+    print(f'disabling_app_bundle_ids: {disabling_app_bundle_ids}')
+    if launched_app_bundle_id not in DEFAULT_DISABLE_BUNDLE_IDS:
+        return
+    was_enabled_globally = actions.speech.enabled()
+    if was_enabled_globally:
+        actions.user.switcher_hide_running()
+        actions.user.history_disable()
+        actions.user.homophones_hide()
+        actions.user.help_hide()
+        actions.speech.disable()
+        disabling_app_bundle_ids.add(launched_app_bundle_id)
+
+def app_quit(app):
+    global was_enabled_globally, disabling_app_bundle_ids
+    quit_app_bundle_id = app.bundle
+    print(f'quit {quit_app_bundle_id}')
+    print(f'was_enabled_globally: {was_enabled_globally}')
+    print(f'disabling_app_bundle_ids: {disabling_app_bundle_ids}')
+    if quit_app_bundle_id not in disabling_app_bundle_ids:
+        return
+    disabling_app_bundle_ids.discard(quit_app_bundle_id)
+    if was_enabled_globally:
+        if len(disabling_app_bundle_ids) == 0:
+            print(f'enabling...')
             actions.speech.enable()
-        if last_active_app_bundle_id in DEFAULT_DISABLE_BUNDLE_IDS:
-            was_enabled_in_app.discard(last_active_app_bundle_id)
-            was_enabled_globally = False
-    last_active_app_bundle_id = active_app_bundle_id
+
+def register_events():
+    ui.register('app_launch', app_launched)
+    ui.register('app_close', app_quit)
 
 # if we try to do this on module load at startup, the action speech.enabled is not yet defined
 from talon import app
-app.register('ready', lambda: ui.register('app_activate', fn))
+app.register('ready', register_events)
