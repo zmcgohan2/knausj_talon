b91c0163f7cef86089043e7841c8e3d69cacb6dc
knausj85 <knaus.jeff@gmail.com>
2021-05-10 16:53:02 -0600

    Update eye_tracking_settings.py

--- eye_tracking_settings.py
+++ eye_tracking_settings.py
@@ -8,8 +8,8 @@ if app.platform == "mac":
     eye_zoom_mouse.config.screen_area = Point2d(width, height)
     eye_zoom_mouse.config.img_scale = 6
 elif app.platform == "windows":
-    width = (int)(ui.main_screen().width * 0.05)
-    height = (int)(ui.main_screen().height * 0.10)
+    width = (int)(ui.main_screen().width * 0.10)
+    height = (int)(ui.main_screen().height * 0.15)
     eye_zoom_mouse.config.screen_area = Point2d(width, height)
     eye_zoom_mouse.config.img_scale = 6
 
