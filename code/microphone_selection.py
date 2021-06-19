52b1695fe694771870c57124ffacf8d09ca88cc7
knausj85 <knaus.jeff@gmail.com>
2021-06-18 23:25:31 -0600

    Update microphone_selection.py

--- code/microphone_selection.py
+++ code/microphone_selection.py
@@ -1,26 +1,11 @@
-from talon import actions
-from talon import Module, actions, imgui, scripting, app
-from talon.microphone import manager
+from talon import Module, actions, app, imgui
 from talon.lib import cubeb
-from talon import scripting
 
 ctx = cubeb.Context()
 mod = Module()
 
 
-def devices_changed(device_type):
-    update_microphone_list()
-
-
 microphone_device_list = []
-
-
-def update_microphone_list():
-    global microphone_device_list
-    microphone_device_list = []
-    for device in ctx.inputs():
-        if str(device.state) == "DeviceState.ENABLED":
-            microphone_device_list.append(device)
 
 
 @imgui.open()
@@ -28,7 +13,7 @@ def gui(gui: imgui.GUI):
     gui.text("Select a Microphone")
     gui.line()
     for index, item in enumerate(microphone_device_list, 1):
-        if gui.button("{}. {}".format(index, item.name)):
+        if gui.button("{}. {}".format(index, item)):
             actions.user.microphone_select(index)
 
 
@@ -39,31 +24,18 @@ class Actions:
         if gui.showing:
             gui.hide()
         else:
+            global microphone_device_list
+            microphone_device_list = [dev.name for dev in ctx.inputs()]
+            microphone_device_list.append("None")
+            microphone_device_list.append("System Default")
+
             gui.show()
 
     def microphone_select(index: int):
         """Selects a micropohone"""
-        # print(str(index) + " " + str(len(microphone_device_list)))
         if 1 <= index and index <= len(microphone_device_list):
-            microphone = microphone_device_list[index - 1].name
-            for item in manager.menu.items:
-                # print(item.name + " " + microphone)
-                if microphone in item.name:
-                    # manager.menu_click(item)
-                    actions.speech.set_microphone(item.name)
-                    app.notify("Activating {}".format(item.name))
-
-                    break
-
+            actions.speech.set_microphone(microphone_device_list[index - 1])
+            app.notify(
+                "Activating microphone: {}".format(microphone_device_list[index - 1])
+            )
             gui.hide()
-
-
-ctx.register("devices_changed", devices_changed)
-
-
-def on_ready():
-    update_microphone_list()
-
-
-app.register("ready", on_ready)
-
