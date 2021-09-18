efa6e0862495b29f6883564e02282e5c48cb68f6
Nicholas Riley <com-github@sabi.net>
2021-09-17 20:12:53 -0400

    Improve formatting of recognized phrase logging.

--- code/history.py
+++ code/history.py
@@ -19,7 +19,7 @@ def parse_phrase(word_list):
 def on_phrase(j):
     global history
 
-    print(j["phrase"])
+    print('>', ' '.join(j["phrase"]))
 
     try:
         val = parse_phrase(getattr(j["parsed"], "_unmapped", j["phrase"]))
