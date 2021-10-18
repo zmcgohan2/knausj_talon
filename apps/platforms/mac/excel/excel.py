46564a3b3b02b85db6fa3b80a402ab3a6f1c136f
Nicholas Riley <com-github@sabi.net>
2021-10-17 21:23:57 -0400

    "Save as XLSX" action for Excel for Mac.

--- apps/platforms/mac/excel/excel.py
+++ apps/platforms/mac/excel/excel.py
@@ -1,4 +1,4 @@
-from talon import Module, Context
+from talon import Module, Context, actions, app, ui
 from talon.mac import applescript
 
 ctx = Context()
@@ -29,3 +29,46 @@ class EditActions:
 		applescript.run(r"""
 			tell application id "com.microsoft.Excel" to set front window's zoom to 100
 		""")
+
+def excel_app():
+	return ui.apps(bundle="com.microsoft.Excel")[0]
+
+def excel_window():
+	return next(window for window in excel_app().windows() if window.doc or window.title)
+
+@ctx.action_class('user')
+class UserActions:
+	def excel_save_as_format(format: str):
+		actions.key('cmd-shift-s')
+		window = excel_window()
+
+		for attempt in range(5):
+			try:
+				sheet = window.children.find_one(AXRole='AXSheet', max_depth=0)
+				break
+			except ui.UIErr:
+				actions.sleep("100ms")
+		else:
+			app.notify(body='Did not find save sheet as expected', title='Excel')
+			return
+
+		file_format_popup = sheet.children.find_one(AXRole='AXPopUpButton', AXDescription='File Format:')
+		file_format_popup.perform('AXPress')
+
+		for attempt in range(5):
+			try:
+				file_format_menu = file_format_popup.children.find_one(AXRole='AXMenu', max_depth=0)
+				break
+			except ui.UIErr:
+				actions.sleep("100ms")
+		else:
+			app.notify(body='Did not find file format menu as expected', title='Excel')
+			return
+
+		file_format_item = file_format_menu.children.find_one(AXRole='AXMenuItem', AXTitle=format, max_depth=0)
+		file_format_item.perform('AXPress')
+
+@mod.action_class
+class Actions:
+	def excel_save_as_format(format: str):
+		"""Save Excel document with format"""
---
+++ apps/platforms/mac/excel/excel.talon
@@ -0,0 +1,3 @@
+app: excel
+-
+save as excel: user.excel_save_as_format('Excel Workbook (.xlsx)')
