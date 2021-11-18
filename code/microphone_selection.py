596295789d56f323d6520f51c229581d97da009e
Jeff Knaus <knaus.jeff@gmail.com>
2021-11-18 05:32:48 -0700

    Add ability to change output audio

--- code/microphone_selection.py
+++ code/microphone_selection.py
@@ -1,16 +1,18 @@
 from talon import Module, actions, app, imgui
 from talon.lib import cubeb
+from subprocess import call
+import os
 
 ctx = cubeb.Context()
 mod = Module()
 
 
 microphone_device_list = []
-
+speaker_device_list = []
 # by convention, None and System Default are listed first
 # to match the Talon context menu.
 def update_microphone_list():
-    global microphone_device_list
+    global microphone_device_list, speaker_device_list
     microphone_device_list = ["None", "System Default"]
 
     # On Windows, it's presently necessary to check the state, or
@@ -18,7 +20,10 @@ def update_microphone_list():
     devices = [
         dev.name for dev in ctx.inputs() if dev.state == cubeb.DeviceState.ENABLED
     ]
-
+    speaker_device_list = [
+        dev.name for dev in ctx.outputs() if dev.state == cubeb.DeviceState.ENABLED
+    ]
+    speaker_device_list.sort()
     devices.sort()
     microphone_device_list += devices
 
@@ -28,7 +33,7 @@ def devices_changed(device_type):
 
 
 @imgui.open()
-def gui(gui: imgui.GUI):
+def gui_microphone(gui: imgui.GUI):
     gui.text("Select a Microphone")
     gui.line()
     for index, item in enumerate(microphone_device_list, 1):
@@ -36,24 +41,74 @@ def gui(gui: imgui.GUI):
             actions.user.microphone_select(index)
 
 
+@imgui.open()
+def gui_speaker(gui: imgui.GUI):
+    gui.text("Select a speaker")
+    gui.line()
+    for index, item in enumerate(speaker_device_list, 1):
+        if gui.button("{}. {}".format(index, item)):
+            actions.user.speaker_select(index)
+
+
 @mod.action_class
 class Actions:
     def microphone_selection_toggle():
         """"""
-        if gui.showing:
-            gui.hide()
+        if gui_microphone.showing:
+            gui_microphone.hide()
         else:
             update_microphone_list()
-            gui.show()
+            gui_microphone.show()
 
     def microphone_select(index: int):
         """Selects a micropohone"""
         if 1 <= index and index <= len(microphone_device_list):
+
             actions.speech.set_microphone(microphone_device_list[index - 1])
             app.notify(
                 "Activating microphone: {}".format(microphone_device_list[index - 1])
             )
-            gui.hide()
+            gui_microphone.hide()
+
+    def speaker_selection_toggle():
+        """"""
+        if gui_speaker.showing:
+            gui_speaker.hide()
+        else:
+            update_microphone_list()
+            gui_speaker.show()
+
+    def speaker_select(index: int):
+        """Selects a speaker"""
+        if 1 <= index and index <= len(speaker_device_list):
+            device_name = speaker_device_list[index - 1]
+            # print("selected thing: " + device_name)
+            if app.platform == "mac":
+                call(
+                    [
+                        "/usr/local/Cellar/switchaudio-osx/1.1.0/SwitchAudioSource",
+                        "-t",
+                        "output",
+                        "-s",
+                        device_name,
+                    ]
+                )
+            elif app.platform == "windows":
+                program_files = os.environ["ProgramFiles"]
+                call(
+                    [
+                        f"{program_files}/nircmd/nircmd.exe",
+                        "setdefaultsounddevice",
+                        device_name,
+                        "1",
+                    ]
+                )
+
+            app.notify("Activating speaker: {}".format(device_name))
+            gui_speaker.hide()
+
+    def system_switch_output_audio(name: str):
+        """Switches output audio"""
 
 
 def on_ready():
--- misc/microphone_selection.talon
+++ misc/microphone_selection.talon
@@ -1,2 +1,4 @@
 ^microphone show$: user.microphone_selection_toggle()
 ^microphone pick <number_small>$: user.microphone_select(number_small)
+^speaker show$: user.speaker_selection_toggle()
+^speaker pick <number_small>$: user.speaker_select(number_small)
