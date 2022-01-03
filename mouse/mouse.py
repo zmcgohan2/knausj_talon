2ee536aaa1c62b5955aedb1b6aa0e23ab7fd1271
Jeff Knaus <knaus.jeff@gmail.com>
2022-01-02 19:00:43 -0700

    Update mouse.py

--- mouse/mouse.py
+++ mouse/mouse.py
@@ -489,14 +489,14 @@ def start_cursor_scrolling():
 if app.platform == "mac":
     from talon import tap
 
-#     def on_move(e):
-#         if not config.control_mouse:
-#             buttons = ctrl.mouse_buttons_down()
-#             # print(str(ctrl.mouse_buttons_down()))
-#             if not e.flags & tap.DRAG and buttons:
-#                 e.flags |= tap.DRAG
-#                 # buttons is a set now
-#                 e.button = list(buttons)[0]
-#                 e.modify()
+    def on_move(e):
+        if not config.control_mouse:
+            buttons = ctrl.mouse_buttons_down()
+            # print(str(ctrl.mouse_buttons_down()))
+            if not e.flags & tap.DRAG and buttons:
+                e.flags |= tap.DRAG
+                # buttons is a set now
+                e.button = list(buttons)[0]
+                e.modify()
 
-#     tap.register(tap.MMOVE | tap.HOOK, on_move)
+    tap.register(tap.MMOVE | tap.HOOK, on_move)
