113735a9202b0ac4c1df244039cfc27bba35f060
Jeff Knaus <knaus.jeff@gmail.com>
2022-02-13 17:44:33 -0700

    Update foot_pedal_zoom_mouse.py

--- hotkeys/foot_pedal_zoom_mouse.py
+++ hotkeys/foot_pedal_zoom_mouse.py
@@ -1,55 +1,12 @@
 from talon import ui, Module, Context, registry, actions, imgui, cron, track
 import sys
 
-mod = Module()
-ctx_zoom_mouse_enabled_use_pedal = Context()
-ctx_zoom_mouse_enabled_use_pedal.matches = r"""
-mode: talon_plugins.eye_zoom_mouse.zoom_mouse_enabled
-and not tag: talon_plugins.eye_zoom_mouse.zoom_mouse_activated
-and tag: talon_plugins.eye_zoom_mouse.zoom_mouse_pedal
-and not user.running: Optikey Mouse
+ctx_zoom_mouse_triggered = Context()
+ctx_zoom_mouse_triggered.matches = r"""
+tag: talon_plugins.eye_zoom_mouse.zoom_mouse_triggered
 """
 
-
-ctx_zoom_mouse_triggered_use_pedal = Context()
-ctx_zoom_mouse_triggered_use_pedal.matches = r"""
-mode: talon_plugins.eye_zoom_mouse.zoom_mouse_enabled
-and not tag: talon_plugins.eye_zoom_mouse.zoom_mouse_activated
-#and tag: talon_plugins.eye_zoom_mouse.zoom_mouse_pedal
-and not user.running: Optikey Mouse
-"""
-
-
-# @ctx_zoom_mouse_enabled_use_pedal.action_class("user")
-# class WindowsZoomMouseInactiveActions:
-#     def foot_pedal_left_left():
-#         """document string goes here"""
-#         actions.talon_plugins.eye_zoom_mouse.mouse_trigger()
-
-#     def foot_pedal_left_middle():
-#         """document string goes here"""
-#         actions.user.toggle_gaze_scroll()
-
-#     def foot_pedal_left_right():
-#         """document string goes here"""
-#         actions.user.system_switcher()
-
-#     def foot_pedal_right_left():
-#         """document string goes here"""
-#         actions.user.mouse_toggle_zoom_mouse()
-#         #actions.key("pageup")
-
-#     def foot_pedal_right_middle():
-#         """document string goes here"""
-#         actions.user.microphone_toggle()
-#         # actions.key("pagedown")
-
-#     def foot_pedal_right_right():
-#         """document string goes here"""
-#         actions.user.wake_or_sleep()
-
-
-@ctx_zoom_mouse_triggered_use_pedal.action_class("user")
+@ctx_zoom_mouse_triggered.action_class("user")
 class WindowsZoomMouseActiveActions:
     def foot_pedal_left_left():
         """document string goes here"""
@@ -74,3 +31,5 @@ class WindowsZoomMouseActiveActions:
     def foot_pedal_right_right():
         """document string goes here"""
         actions.talon_plugins.eye_zoom_mouse.mouse_move()
+
+
